f8eee96beef430242d4801396c94fbe4eab16f13
Johan Tsung <johan.tsung@seb.se>
2021-07-08 12:44:28 +0200

    functionallty console app added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,14 @@
 ﻿using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcService;
+using IdentityModel.Client;
+using RestSharp;
+using RestSharp.Authenticators;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using static GrpcService.Greeter;
+using Newtonsoft.Json.Linq;
 
 namespace ConsoleApp
 {
@@ -12,13 +17,29 @@ namespace ConsoleApp
         static async Task Main(string[] args)
         {
 
-            var _token = "22222eyJhbGciOiJSUzI1NiIsImtpZCI6IjNGRTYyODE4RTJCNjc3OEY3Nzg2NjAxMjc2RkM5Qzg5IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MjU2NjQxOTIsImV4cCI6MTYyNTY2Nzc5MiwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NDQzOTAiLCJhdWQiOiJhcGkxIiwiY2xpZW50X2lkIjoib2F1dGhDbGllbnQiLCJqdGkiOiJGOTk3NTE2MTNCRjA4NzlEODJCMjhEQjgxNTAxMUUxOCIsImlhdCI6MTYyNTY2NDE5Miwic2NvcGUiOlsiYXBpMS5yZWFkIl19.fyGagZ2oB7T77U5P-5aQ_Z4fkK-CRR6sQBDibub8oX1ckuR5DuQYzKpN5GTqeimfEWfvG9aVvK9e9-ZkM7ocBy3PgaUdwERCxXO60HbZUk_wisK39w-vhzHKEipeWYkVyTv-wM2HhLUe5j3_P7MZJYx6vGYgBOa1Ilxq5kxYDbE15F6fTfJoGmxfSns2jww_dTdkUlCrxDFUVDaQZpi6ECOiCyYXa5QX7snlHz5Ick1sfrjtWuEtAGgJQNJMHLCPOnJwGvvChj5OaroFnY7ZLsHfbHRo_KrxwVtfeuJD-RxFKVDwa5wrP9u6ZmmqQndvdDrF5MGMnPpYq31dZO8W4g";
+            
+            //var _token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNGRTYyODE4RTJCNjc3OEY3Nzg2NjAxMjc2RkM5Qzg5IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MjU3Mzk2MzUsImV4cCI6MTYyNTc0MzIzNSwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NDQzOTAiLCJhdWQiOiJhcGkxIiwiY2xpZW50X2lkIjoib2F1dGhDbGllbnQiLCJqdGkiOiI5NDc4NkYwNEQ4N0ZBRjM3MTIxQjlCMUYwRTAwQzU3QyIsImlhdCI6MTYyNTczOTYzNCwic2NvcGUiOlsiYXBpMS5yZWFkIl19.iXDMWKW-5eUx5sY0lDm9dDN6VoSUe3nR4RvwVLkQUa3MmesK_97raTtvgDCGkTsj6gRhe6K9AcQxLE2aA617sM2QtmRVsWmjo1zxQiwJ6eaooMVDpjrMnd-4EI76-V28COW8ifxc7CdkiCUWdds_2W55JG7kRpWx7yIAwPIPuoiXceN9X6sHIB2mfmcFqgRjsbzWKbdSWyai5WdPMOCRNGYFTqAnToKISBBMh0d3igXiRr4HMNQ_u0bDpyeHP7lq4ZoO0mdGN1y7vklAMjXr95W9IdJ41V9f9zSx5GkF8435SfPeDoYruAf82tS5Kh-zH4uHMi-HN8oO9LuWhbNlVA";
+
+
+            ///* using RestSharp; // https://www.nuget.org/packages/RestSharp/ */
+            var client2 = new RestClient("https://localhost:44390/connect/token");
+            var request2 = new RestRequest(Method.POST);
+            request2.AddHeader("cache-control", "no-cache");
+            request2.AddHeader("content-type", "application/x-www-form-urlencoded");
+            request2.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&client_id=oauthClient&client_secret=SuperSecretPassword", ParameterType.RequestBody);
+            IRestResponse response2 = client2.Execute(request2);
+
+            dynamic resp = JObject.Parse(response2.Content);
+            var _token = resp.access_token;
+            //Console.WriteLine("token: " + _token);
+
+
             var headers = new Metadata();
             headers.Add("Authorization", $"Bearer {_token}");
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello gRPC service!");
             Console.WriteLine("=====================================");
-            Console.WriteLine("Wait for response");
+            Console.WriteLine("Wait for response ... ");
             Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine("");
@@ -48,7 +69,14 @@ namespace ConsoleApp
 
         }
 
-           
+
+    
+
+
+
+
+
+
 
     }
 }
