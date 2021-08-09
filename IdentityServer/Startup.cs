4d3d342019af015441b1a1750a6572f006282d3a
s6821b <johan.tsung@seb.se>
2021-08-09 16:43:23 +0200

    Identiserver greeting! added

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -42,7 +42,7 @@ namespace IdentityServer4.Example
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! \r\n\r\n\r\n\tfrom\r\n\tIdentityServer4");
                 });
             });
         }
