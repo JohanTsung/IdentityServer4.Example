20df1c9bd8fe9986fa561b119a3de92d678f4e28
s6821b <johan.tsung@seb.se>
2021-08-11 13:26:15 +0200

    Timout and selection added

---
+++ ConsoleApp/GetToken.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class GetToken
+    {
+        public static string getToken(string ID4url) {
+            var _token = "";
+            try
+            {
+                Console.WriteLine("Getting toknen ...");
+                Console.WriteLine("\r\n=====================================");
+                Console.WriteLine("  ID4 server ... conecting to ");
+                Console.WriteLine("\t{0}", ID4url);
+                ///* using RestSharp; // https://www.nuget.org/packages/RestSharp/ */
+                var client2 = new RestClient(ID4url);
+                var request2 = new RestRequest(Method.POST);
+                request2.AddHeader("cache-control", "no-cache");
+                request2.AddHeader("content-type", "application/x-www-form-urlencoded");
+                request2.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&scope=api1.read&client_id=oauthClient&client_secret=SuperSecretPassword", ParameterType.RequestBody);
+                IRestResponse response2 = client2.Execute(request2);
+
+                dynamic resp = JObject.Parse(response2.Content);
+                _token = resp.access_token;
+                string utNme = _token.ToString().Substring(0, 12);
+
+                Console.WriteLine("\r\ntoken: " + utNme + " .... \r\n\r\n");
+            }
+            catch (Exception e)
+            {
+                //code for any other type of exception
+                Console.WriteLine(e.Message);
+                _token = "";
+            }
+
+            string returntoken = _token.ToString();
+            return returntoken;
+        }
+
+ 
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,49 +9,73 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using static GrpcService.Greeter;
 using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace ConsoleApp
 {
     class Program
     {
+                    
+
         static async Task Main(string[] args)
         {
 
+            // URL to Identity server
+            string ID4url = "https://localhost:44390/connect/token";
+            string _token = "";
 
-            Console.WriteLine("Getting toknen ...");
+            
+            //string temp_token = Console.ReadLine();
+            string temp_token = "";
             //var _token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNGRTYyODE4RTJCNjc3OEY3Nzg2NjAxMjc2RkM5Qzg5IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MjU3Mzk2MzUsImV4cCI6MTYyNTc0MzIzNSwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NDQzOTAiLCJhdWQiOiJhcGkxIiwiY2xpZW50X2lkIjoib2F1dGhDbGllbnQiLCJqdGkiOiI5NDc4NkYwNEQ4N0ZBRjM3MTIxQjlCMUYwRTAwQzU3QyIsImlhdCI6MTYyNTczOTYzNCwic2NvcGUiOlsiYXBpMS5yZWFkIl19.iXDMWKW-5eUx5sY0lDm9dDN6VoSUe3nR4RvwVLkQUa3MmesK_97raTtvgDCGkTsj6gRhe6K9AcQxLE2aA617sM2QtmRVsWmjo1zxQiwJ6eaooMVDpjrMnd-4EI76-V28COW8ifxc7CdkiCUWdds_2W55JG7kRpWx7yIAwPIPuoiXceN9X6sHIB2mfmcFqgRjsbzWKbdSWyai5WdPMOCRNGYFTqAnToKISBBMh0d3igXiRr4HMNQ_u0bDpyeHP7lq4ZoO0mdGN1y7vklAMjXr95W9IdJ41V9f9zSx5GkF8435SfPeDoYruAf82tS5Kh-zH4uHMi-HN8oO9LuWhbNlVA";
+            
 
 
-
-
-            Console.WriteLine("\r\n=====================================");
-            Console.WriteLine("  ID4 server ... conecting to ");
-            ///* using RestSharp; // https://www.nuget.org/packages/RestSharp/ */
-            var url = "https://localhost:44390/connect/token";
-            Console.WriteLine("\t{0}",  url);
-            var _token = "";
-            try
-            {                
-            var client2 = new RestClient(url);
-            var request2 = new RestRequest(Method.POST);
-            request2.AddHeader("cache-control", "no-cache");
-            request2.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request2.AddParameter("application/x-www-form-urlencoded", "grant_type=client_credentials&scope=api1.read&client_id=oauthClient&client_secret=SuperSecretPassword", ParameterType.RequestBody);
-            IRestResponse response2 = client2.Execute(request2);
-
-            dynamic resp = JObject.Parse(response2.Content);
-            _token = resp.access_token;                
-            string utNme = _token.ToString().Substring(0, 12);
-
-                Console.WriteLine("\r\ntoken: " + utNme + " .... \r\n\r\n");
-            }
-            catch (Exception e)
+            // timeout
+            ConsoleKeyInfo k = new ConsoleKeyInfo();            
+            for (int cnt = 7; cnt > 0; cnt--)
             {
-                //code for any other type of exception
-                Console.WriteLine(e.Message);
-                _token = "";
+                if (Console.KeyAvailable == true)
+                {
+                    k = Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Press any key to enter a token.");
+                    Console.WriteLine("Timeout :  {0} s", cnt.ToString());
+                }
             }
 
+            //Console.WriteLine("The key pressed was " + k.Key);
+
+            if (k.Key.ToString() == "0") { 
+                Console.WriteLine("No key pressed ... continue");
+            } else {
+                Console.WriteLine("The key pressed was " + k.Key);
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("Enter token to continue");
+                Console.WriteLine("(Leave empty to retive token for ID4 server)\r\n");
+                temp_token = Console.ReadLine();
+            }
+
+
+
+
+
+            // getting token from server or manually insert token.
+            if (temp_token == "") {
+                Console.WriteLine("(2) Getting token from ID4 server ...\r\n");
+                _token = GetToken.getToken(ID4url);
+            } else {
+                Console.WriteLine("(1) Token Retrieved!\r\n");
+                _token = temp_token;
+                temp_token = _token.ToString().Substring(0, 12);
+                Console.WriteLine("\r\ntoken: " + temp_token + " .... \r\n\r\n");
+            }
+
+ 
 
             var headers = new Metadata();
             headers.Add("Authorization", $"Bearer {_token}");
